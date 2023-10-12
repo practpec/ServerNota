e7ce79b007c645382afd382726f0402a56f842d0
practpec <j08433307@gmail.com>
2023-10-12 11:29:23 -0600

    Correccion para Vercel

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 db.connect((err) => {
   if (err) {
